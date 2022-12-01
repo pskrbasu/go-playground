f1b390200990cf5e25cc76b03bbf595b1143468b
Puskar Basu <puskar@turbot.com>
2022-12-01 20:02:11 +0530

    compare json files

--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module cgo-test
 
 go 1.17
+
+require github.com/wI2L/jsondiff v0.3.0
+
+require (
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
+
+require (
+	github.com/josephburnett/jd v1.6.1
+	github.com/tidwall/gjson v1.14.3 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
-// #include <stdio.h>
-// #include <stdlib.h>
-//
-// static void myprint(char* s) {
-//   printf("%s\n", s);
-// }
-import "C"
-import "unsafe"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"github.com/wI2L/jsondiff"
+)
 
 func main() {
-	cs := C.CString("Hello from stdio")
-	C.myprint(cs)
-	C.free(unsafe.Pointer(cs))
+	source, err := ioutil.ReadFile("source_files/source.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	target, err := ioutil.ReadFile("source_files/target.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	patch, err := jsondiff.CompareJSON(source, target)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var flag int
+	for _, op := range patch {
+		fmt.Printf("%s\n", op)
+		if op.Path.String() == "/end_time" || op.Path.String() == "/start_time" || strings.Contains(op.Path.String(), "/search_path") {
+			continue
+		} else {
+			flag = 1
+		}
+	}
+	if flag == 0 {
+		fmt.Println("identical")
+	} else {
+		fmt.Printf("not identical")
+	}
 }
