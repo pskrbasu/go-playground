3314cf34ffe80c3e2acb8ede133f8ee03289f8a6
Puskar Basu <puskar@turbot.com>
2023-02-01 19:53:17 +0530

    sample input program

--- main.go
+++ main.go
@@ -1,39 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"strings"
-
-	"github.com/wI2L/jsondiff"
-)
+import "fmt"
 
 func main() {
-	source, err := ioutil.ReadFile("source_files/source.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	target, err := ioutil.ReadFile("source_files/target.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	patch, err := jsondiff.CompareJSON(source, target)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var flag int
-	for _, op := range patch {
-		fmt.Printf("%s\n", op)
-		if op.Path.String() == "/end_time" || op.Path.String() == "/start_time" || strings.Contains(op.Path.String(), "/search_path") {
-			continue
-		} else {
-			flag = 1
-		}
-	}
-	if flag == 0 {
-		fmt.Println("identical")
-	} else {
-		fmt.Printf("not identical")
-	}
+	var name string
+
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&name)
+
+	fmt.Printf("Name: %s", name)
 }
