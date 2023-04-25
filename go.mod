ff25be9b6330328f72da96cb7938a0c0050aa941
Puskar Basu <puskar@turbot.com>
2023-04-25 10:43:02 +0530

    added shopify program

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.17
 require github.com/wI2L/jsondiff v0.3.0
 
 require (
+	github.com/bold-commerce/go-shopify/v3 v3.14.0
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/tidwall/gjson v1.14.3 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	goshopify "github.com/bold-commerce/go-shopify/v3"
+)
 
 func main() {
-	var name string
+	// Create an app somewhere.
+	// app := goshopify.App{
+	// 	ApiKey:    "a85458ebf7b2797e0516a0410481a15e",
+	// 	ApiSecret: "2694c8452c89896986063f1f69be11b2",
+	// }
+	// Create a new API client
+	client := goshopify.NewClient(goshopify.App{}, "test-turbot-pc-1", "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")
+	// Fetch the number of products
+	numProducts, err := client.Product.Count(nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Total number of products: %v\n\n", numProducts)
 
-	fmt.Print("Enter your name: ")
-	fmt.Scan(&name)
+	// fetch the products
+	products, _, err := client.Product.ListWithPagination(nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(">>>> Products: ")
+	for _, prod := range products {
+		fmt.Println(prod.AdminGraphqlAPIID)
+	}
 
-	fmt.Printf("Name: %s", name)
+	// fetch the orders
+	orders, err := client.Order.List(nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(">>>> Orders: ")
+	for _, ord := range orders {
+		fmt.Println(ord.Name)
+	}
+
+	// or, err := client.Order.Get(5367225057575, nil)
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf(">>>> order detail: %v", or.CreatedAt)
+	// fmt.Println()
+
+	// fetch the customers
+	// customers, err := client.Customer.List(nil)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(">>>> Customers: ")
+	// for _, cus := range customers {
+	// 	fmt.Println(cus.Metafields)
+	// }
+
+	// fetch the companies
+	// companies, err := client.Co
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(">>>> Customers: ")
+	// for _, cus := range customers {
+	// 	fmt.Println(cus.FirstName)
+	// }
+
 }
