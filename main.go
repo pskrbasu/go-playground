22b42d86587e33ac54b073676d254cfd2dc71d53
Puskar Basu <puskar@turbot.com>
2024-01-15 15:37:59 +0530

    cgo

--- main.go
+++ main.go
@@ -1,72 +1,16 @@
 package main
 
-import (
-	"fmt"
-
-	goshopify "github.com/bold-commerce/go-shopify/v3"
-)
+// #include <stdio.h>
+// #include <stdlib.h>
+//
+// static void myprint(char* s) {
+//   printf("%s\n", s);
+// }
+import "C"
+import "unsafe"
 
 func main() {
-	// Create an app somewhere.
-	// app := goshopify.App{
-	// 	ApiKey:    "",
-	// 	ApiSecret: "",
-	// }
-	// Create a new API client
-	client := goshopify.NewClient(goshopify.App{}, "test-turbot-pc-1", "")
-	// Fetch the number of products
-	numProducts, err := client.Product.Count(nil)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Total number of products: %v\n\n", numProducts)
-
-	// fetch the products
-	products, _, err := client.Product.ListWithPagination(nil)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(">>>> Products: ")
-	for _, prod := range products {
-		fmt.Println(prod.AdminGraphqlAPIID)
-	}
-
-	// fetch the orders
-	orders, err := client.Order.List(nil)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(">>>> Orders: ")
-	for _, ord := range orders {
-		fmt.Println(ord.Name)
-	}
-
-	// or, err := client.Order.Get(5367225057575, nil)
-
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf(">>>> order detail: %v", or.CreatedAt)
-	// fmt.Println()
-
-	// fetch the customers
-	// customers, err := client.Customer.List(nil)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(">>>> Customers: ")
-	// for _, cus := range customers {
-	// 	fmt.Println(cus.Metafields)
-	// }
-
-	// fetch the companies
-	// companies, err := client.Co
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(">>>> Customers: ")
-	// for _, cus := range customers {
-	// 	fmt.Println(cus.FirstName)
-	// }
-
+	cs := C.CString("Hello World!")
+	C.myprint(cs)
+	C.free(unsafe.Pointer(cs))
 }
